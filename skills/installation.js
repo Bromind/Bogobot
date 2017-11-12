7116b7113bc8f521394d6587bd63fd753a9d3331
afgbloch <bloch.aurelien@gmail.com>
2017-11-12 03:42:49 +0100

    add installation skill

---
+++ skills/installation.js
@@ -0,0 +1,40 @@
+//
+// Command: installation
+//
+
+var util = require('../utils/util.js')
+var db = require('../utils/db.js')
+
+module.exports = function (controller, bot) {
+
+    controller.hears([
+        /^installation of (.*) (.*) in room (.*)$/,
+        /^install (.*) (.*) in room (.*)$/,
+        ], 'direct_message,direct_mention', function (bot, message) {
+        util.protected(bot, message, installation);
+    });
+}
+
+function installation(bot, message, usr) {
+    var first_name = message.match[1];
+    var last_name = message.match[2];
+    var room = message.match[3];
+    var patient = { first_name : first_name, last_name : last_name };
+
+    db.update(patient,
+    { $set: { room:room, ref_prof:usr.user } }, {}, function(err, numAffected) {
+        if (err || numAffected != 1) {
+            bot.reply(message, "User not found");
+        } else {
+            db.update(usr,
+            {$push : {patients:patient}}, {}, function(err, numAffected) {
+                if (err || numAffected != 1) {
+                    bot.reply(message, "User not found");
+                } else {
+                    bot.reply(message, "installation succeed");
+                }
+            })
+        }
+    })
+
+}
