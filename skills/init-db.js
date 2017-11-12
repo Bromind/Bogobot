a45a581316a432c8e5c88739542d735a935f507e
Martin Vassor <martin.vassor@epfl.ch>
2017-11-12 02:36:24 +0100

    add Aurelien userId

--- skills/init-db.js
+++ skills/init-db.js
@@ -9,6 +9,7 @@ module.exports = function (controller, bot) {
     controller.hears([/^init-db$/], 'direct_message,direct_mention', function (bot, message) {
         var tab = [];
         tab[0] = {
+            "userId":"Y2lzY29zcGFyazovL3VzL1BFT1BMRS80YWNjYWMzMy00NTU0LTRjYjEtOTM1Ny0yYzNhODY0MzczZTM",
             "user":"aurelien.bloch@epfl.ch",
             "first_name":"Aurelien",
             "last_name":"Bloch",
@@ -40,7 +41,7 @@ module.exports = function (controller, bot) {
             "prof":false
         }
         tab[3] = {
-            "user":"scheldon.cooper@epfl.ch",
+            "user":"scheldon.cooper@caltech.edu",
             "first_name":"Scheldon",
             "last_name":"Cooper",
             "_id":"KF0dlXYP82MdHKM9",
